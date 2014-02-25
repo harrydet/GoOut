2f0d4280ffdea686a370553b3db820ee28ea9582
Harry <harry.det@hotmail.com>
2014-02-25 23:23:08 +0000

    Polished main menu. Added second activity

--- app/src/main/java/com/example/gooutportsmouth/app/MainActivity.java
+++ app/src/main/java/com/example/gooutportsmouth/app/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.gooutportsmouth.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
+
+    //Button animation variable
+    Animation animAlpha;
+    TextView town1;
+    TextView town2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,26 +25,29 @@ public class MainActivity extends ActionBarActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_main);
+
+        setupWidgets();
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onClick(View v) {
+        if(v.getId() == R.id.town1text){
+            v.startAnimation(animAlpha);
+        }
+
+        if(v.getId() == R.id.town2text){
+            v.startAnimation(animAlpha);
+        }
     }
 
-    //test
+    public void setupWidgets(){
+        town1 = (TextView) findViewById(R.id.town1text);
+        town1.setOnClickListener(this);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+        town2 = (TextView) findViewById(R.id.town2text);
+        town2.setOnClickListener(this);
+
+        animAlpha = AnimationUtils.loadAnimation(this, R.anim.anim_alpha);
     }
-
 }
---
+++ app/src/main/java/com/example/gooutportsmouth/app/PortsmouthClubs.java
@@ -0,0 +1,15 @@
+package com.example.gooutportsmouth.app;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class PortsmouthClubs extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portsmouth_clubs);
+    }
+
+
+}
